63c21794baa78497ae9ade7fbd90f8c539ead93c
Yadwinder Pal Singh <contact@yadwindersingh.net>
2016-10-28 14:02:59 -0400

    initial commit
    Added layout, home and store, custom routes

---
+++ ShopTimeMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ShopTimeMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Men",
+                url: "Men",
+                defaults: new { controller = "Store", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Women",
+                url: "Women",
+                defaults: new { controller = "Store", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Kids",
+                url: "Kids",
+                defaults: new { controller = "Store", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ShopTimeMVC/Controllers/StoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopTimeMVC.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Men
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
